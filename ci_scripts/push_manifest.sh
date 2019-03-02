f347dfdc68b13776eed1a341e062605fd33b7323
Jimmy Lafontaine Rivera <lafonj@gmail.com>
2019-03-02 08:58:05 -0400

    Move to new Cachengo infra

--- ci_scripts/push_manifest.sh
+++ ci_scripts/push_manifest.sh
@@ -1,4 +1,5 @@
 export IMAGE_TAG=$(cat VERSION)
+export DOCKER_CLI_EXPERIMENTAL=enabled
 
 docker manifest create --amend cachengo/onos-synchronizer:$IMAGE_TAG cachengo/onos-synchronizer-x86_64:$IMAGE_TAG cachengo/onos-synchronizer-aarch64:$IMAGE_TAG
 
